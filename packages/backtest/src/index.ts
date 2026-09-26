9c3d6564196576b22ea37847eb4f108713691b4c
nebulyn05 <vo.ugwude+nebulyn@stu.unizik.edu.ng>
2026-09-26 19:00:27 +0100

    Remove backtesting package

--- packages/backtest/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./types.js";
-export * from "./metrics.js";
-export * from "./rule-filter.js";
-export * from "./sizing-replay.js";
-export * from "./replay.js";
-export * from "./load-history.js";
